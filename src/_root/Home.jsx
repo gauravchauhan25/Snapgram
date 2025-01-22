695255e10a11c5a5445f3faa04fe19f550fe3966
Gaurav Singh Chauhan <gkumarc121@gmail.com>
2025-01-22 13:07:13 +0530

    Update Home.jsx

--- src/_root/Home.jsx
+++ src/_root/Home.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React , {useEffect} from "react";
 import "../App.css";
 import Feed from "../components/Feed";
 import Stories from "../components/Stories";
 
 export default function Home() {
+  useEffect(() => {
+    document.title = "Home";
+  }, []);
+
   return (
     <>
       <div className="middle">
